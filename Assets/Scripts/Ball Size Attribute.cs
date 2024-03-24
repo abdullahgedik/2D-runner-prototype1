b194f6f34ba5539ab64df415d2343b0c442fe25e
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-03-24 22:45:37 +0300

    Update PractiseScene

---
+++ Assets/Scripts/Ball Size Attribute.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BallSizeAttribute : MonoBehaviour
+{
+    [SerializeField]
+    [ColorUsage(true, true)]
+    private Color red = Color.red;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
